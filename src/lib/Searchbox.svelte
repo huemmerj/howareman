5efa1bcf78340e4c96b2f33a36f9d3dd57243d31
Jens Hümmer <huemmer.jens@gmail.com>
2023-09-29 15:47:13 +0200

    added Search Bar

---
+++ src/lib/Searchbox.svelte
@@ -0,0 +1,4 @@
+<div class="flex justify-center p-5">
+    <input class="border rounded-l-md py-2 pl-1 "placeholder="nach was Suchst du?"/>
+    <button class="border rounded-r-md">Scannen</button>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
+	import Searchbox from "$lib/Searchbox.svelte";
     import Footer from "$lib/navigation/Footer.svelte";
 import "../app.css";    
 </script>
-
+<Searchbox/>
 <slot />
-
 <Footer/>
