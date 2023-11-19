a937c03c4935b86f8d93bcad8bd96e6d2bd2ec04
Jens Hümmer <huemmer.jens@gmail.com>
2023-11-19 23:27:25 +0100

    as Article[]

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,7 @@
 import { collections } from '$db/mongo';
+import type Article from '../models/article';
 export async function load(): Promise<{ data: string }> {
-	const article = await collections.articles?.find().toArray();
-	console.log(article);
-	return { data: JSON.stringify(article) };
+	const articles = (await collections.articles?.find().toArray()) as Article[];
+	console.log(articles);
+	return { data: JSON.stringify(articles) };
 }
