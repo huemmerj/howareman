fe71f60c10a03ee8b9dcf5ed9a419e49eabb353a
Jens Hümmer <huemmer.jens@gmail.com>
2023-12-05 00:13:52 +0100

    fix footer on /create

--- src/lib/navigation/FooterItem.svelte
+++ src/lib/navigation/FooterItem.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-    export let name: string;
-    export let href: string;
-    import { page } from '$app/stores';
+	export let name: string;
+	export let href: string;
+	import { page } from '$app/stores';
 </script>
 
 <div
-    class=" flex flex-col text-center flex-1 items-center"
-    class:text-primary={$page.route.id == href}
-    class:text-text1={$page.route.id !== href}>
-    <slot />
-    <a class="p-1 pb-2" {href}>{name}</a>
+	class=" flex flex-col text-center flex-1 items-center"
+	class:text-primary={$page.route.id?.includes(href)}
+	class:text-text1={!$page.route.id?.includes(href)}
+>
+	<slot />
+	<a class="p-1 pb-2" {href}>{name}</a>
 </div>
