308d536d64ee6b94f20622f90d99a7d79a6b8681
Jens Hümmer <huemmer.jens@gmail.com>
2023-12-13 11:47:29 +0100

    show Articles on Article Count

--- src/lib/article/ArticleItem.svelte
+++ src/lib/article/ArticleItem.svelte
@@ -7,10 +7,11 @@
 	import EditButton from '$lib/buttons/IconEditButton.svelte';
 	import type Article from '../../models/article';
 	export let article: Article;
+	export let small: boolean = false;
 </script>
 
 <div class="shadow-md flex border border-tertiary rounded-lg">
-	<div class="p-3 flex-auto w-32">
+	<div class="p-3 flex-auto">
 		<img class="rounded-lg" src="https://via.placeholder.com/150" alt="Artikelbild" />
 	</div>
 	<div class="flex-row flex-auto pt-2 w-64">
@@ -19,10 +20,13 @@
 			<div class="text-sm text-text2">{article.description}</div>
 			<div class="text-xs text-text3">{article.articleNumber}</div>
 		</div>
-		<div class="flex gap-3 pt-1">
-			<EditButton />
-			<DeleteButton on:click={() => dispatch('delete', 1)} />
-			<AddToOrderListButton on:click={() => dispatch('addToOrderList')} />
-		</div>
+		{#if !small}
+			<div class="flex gap-3 pt-1">
+				<EditButton />
+				<DeleteButton on:click={() => dispatch('delete', 1)} />
+				<AddToOrderListButton on:click={() => dispatch('addToOrderList')} />
+			</div>
+		{/if}
+
 	</div>
 </div>
--- src/lib/dialogs/ArticleCountDialog.svelte
+++ src/lib/dialogs/ArticleCountDialog.svelte
@@ -3,10 +3,27 @@
 	import Dialog from './Dialog.svelte';
 	import Counter from '$lib/buttons/Counter.svelte';
 	import Searchbox from '$lib/Searchbox.svelte';
+	import ArticleItem from '$lib/article/ArticleItem.svelte';
+	import { onMount } from 'svelte';
+	import { scannedArticleNumber } from '../../Store';
+	import A from '$lib/buttons/A.svelte';
 
-	export let showModal: Boolean; // boolean
+	export let showModal: Boolean;
 
 	export let dialog: HTMLDialogElement;
+
+	$: articles = [];
+
+	$: articleNumber = '';
+	onMount(() => {
+		scannedArticleNumber.subscribe(async(e) => {
+			if (!e) return;
+			const res = await fetch(`/article?articleNumber=${e}`);
+			articles = await res.json();
+			articleNumber = e as string
+			scannedArticleNumber.set('');
+		});
+	});
 </script>
 
 <Dialog bind:dialog on:close={() => console.log('closed')} bind:showModal>
@@ -14,7 +31,22 @@
 		<p class="text-text1">Artikel zählen</p>
 	</div>
 	<div slot="body">
-		<Searchbox showSearchButton={false}/>
+		<div class="pb-2.5">
+			<Searchbox showSearchButton={false}/>
+		</div>
+		<div class="flex flex-col gap-5">
+
+		{#each articles as article}
+			<ArticleItem
+				{article}
+				small={true}
+			/>
+		{/each}
+		<ArticleItem
+			article={{uuid:"3", name: 'Test', description: 'Test', articleNumber: '123'}}
+			small={true}
+		/>
+			</div>
 	</div>
 	<div slot="footer">
 		<CancleButton on:click={() => dialog.close()} />
