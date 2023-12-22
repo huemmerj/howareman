c9bc6114f18eaa6713236cbb9058bd442024e86f
Jens Hümmer <huemmer.jens@gmail.com>
2023-12-22 08:31:54 +0100

    fixed count articles

--- src/routes/stocktaking/count/[id]/+server.ts
+++ src/routes/stocktaking/count/[id]/+server.ts
@@ -1,49 +1,54 @@
 import { collections } from '$db/mongo';
+import { error } from '@sveltejs/kit';
 // import { stockTaking } from '$lib/dialogs/ArticleCountDialog.svelte';
 export async function PUT({ params, request }) {
-	const body = await request.json();
+	try {
+		const body = await request.json();
+		for (const countedArticle of body) {
+			const { uuid, count, name } = countedArticle;
+			const { id } = params;
+			const stockTaking = await collections.stockTaking?.findOne({
+				uuid: id
+			});
 
-	for (const countedArticle of body) {
-		const { uuid, count, name } = countedArticle;
-		const { id } = params;
-		const stockTaking = await collections.stockTaking?.findOne({
-			uuid: id
-		});
-
-		if (!stockTaking) {
-			return new Response(null, { status: 404 });
-		}
-		console.log(stockTaking);
-		const article = stockTaking.articles?.find((article) => article.uuid === uuid);
-		console.log(article);
-		if (article) {
-			await collections.stockTaking?.updateOne(
-				{
-					uuid: id,
-					'articles.uuid': uuid
-				},
-				{
-					$set: {
-						'articles.$.count': count
-					}
-				}
-			);
-		} else {
-			collections.stockTaking?.updateOne(
-				{
-					uuid: id
-				},
-				{
-					$push: {
-						articles: {
-							uuid,
-							count,
-							name
+			if (!stockTaking) {
+				return new Response(null, { status: 404 });
+			}
+			console.log(stockTaking);
+			const article = stockTaking.articles?.find((article) => article.uuid === uuid);
+			console.log(article);
+			if (article) {
+				await collections.stockTaking?.updateOne(
+					{
+						uuid: id,
+						'articles.uuid': uuid
+					},
+					{
+						$set: {
+							'articles.$.count': count
 						}
 					}
-				}
-			);
+				);
+			} else {
+				collections.stockTaking?.updateOne(
+					{
+						uuid: id
+					},
+					{
+						$push: {
+							articles: {
+								uuid,
+								count,
+								name
+							}
+						}
+					}
+				);
+			}
 		}
+		return new Response(null, { status: 201 });
+	} catch (e) {
+		console.log(e);
+		return error(400);
 	}
-	return new Response(null, { status: 204 });
 }
