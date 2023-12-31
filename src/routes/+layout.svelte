c129d5390c2b3ded9392385adad9fde2ca90fe6b
Jens Hümmer <huemmer.jens@gmail.com>
2023-12-31 20:31:22 +0100

    removed signin button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,15 @@ import ScanDialog from '$lib/dialogs/ScanDialog.svelte';
   import Footer from '$lib/navigation/Footer.svelte';
 	import '../app.css';
 
+	import UserButton from 'clerk-sveltekit/client/UserButton.svelte'
+	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte'
+	import SignedOut from 'clerk-sveltekit/client/SignedOut.svelte'
 </script>
 
 <div>
+	<SignedIn>
+		<UserButton afterSignOutUrl="/login" />
+	</SignedIn>
 	<slot />
 	<Footer />
 	<ScanDialog />
